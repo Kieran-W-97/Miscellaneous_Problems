3d2394d967cab65a8eca847789bbe21a524bae39
Kieran-W-97 <kieranwachsmuth@gmail.com>
2020-06-30 13:28:17 +0100

    fix bugs. Produces permuations given total length of array and the list of Nonogram values.

--- permutator.py
+++ permutator.py
@@ -16,9 +16,17 @@ def permutate(N,lst):
     box_sizes = []
     for i in range(len(lst)+1):
         box_sizes.append(N_zeros)
+    solutions = []
     for sol in list(box_perm(N_zeros,box_sizes)):
         # now generate the row/column solution:
-        for j in range(len(sol)):
-        solution = list(np.zeros(sol[])) + list(np.ones(lst[]))
-
-permutate(6,[2,2])
+        solution = list(np.zeros(sol[0],dtype=int))
+        for j in range(len(sol)-2):
+            solution += list(np.ones(lst[j],dtype=int))
+            solution += [0]
+            solution += list(np.zeros(sol[j+1],dtype=int))
+        solution += list(np.ones(lst[-1],dtype=int))
+        solution += list(np.zeros(sol[-1],dtype=int))
+        solutions.append(solution)
+    return solutions
+sols = permutate(6,[2,2])
+print(sols)
